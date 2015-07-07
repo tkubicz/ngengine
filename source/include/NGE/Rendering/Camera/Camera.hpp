523b01a55dbff5580cc8965d0bdab28b24bdd9fe
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2015-07-07 02:44:09 +0200

    refs #38 @1h30min Working moving forward by user input. Modyfication in Application class to support touch input.

--- source/include/NGE/Rendering/Camera/Camera.hpp
+++ source/include/NGE/Rendering/Camera/Camera.hpp
@@ -12,60 +12,64 @@
 #include "NGE/Math/Vector3.hpp"
 #include "NGE/Math/Matrix4.hpp"
 #include "NGE/Events/KeyboardEvent.hpp"
+#include "NGE/Events/TouchEvent.hpp"
 #include "NGE/Rendering/Camera/Frustum.hpp"
 
 namespace NGE {
-	namespace Rendering {
-		namespace Camera {
+    namespace Rendering {
+        namespace Camera {
 
-			class Camera {
-			  public:
-				Camera(float positionX = 0.0f, float positionY = 0.0f, float positionZ = 0.0f,
-						float focusX = 0.0f, float focusY = 0.0f, float focusZ = 0.0f,
-						float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f);
+            class Camera {
+              public:
+                Camera(float positionX = 0.0f, float positionY = 0.0f, float positionZ = 0.0f,
+                       float focusX = 0.0f, float focusY = 0.0f, float focusZ = 0.0f,
+                       float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f);
 
-				void Set(const Math::vec3f& position, const Math::vec3f& focus, const Math::vec3f& up);
-				void Set(float positionX, float positionY, float positionZ,
-						float focusX, float focusY, float focusZ,
-						float upX, float upY, float upZ);
+                void Set(const Math::vec3f& position, const Math::vec3f& focus, const Math::vec3f& up);
+                void Set(float positionX, float positionY, float positionZ,
+                         float focusX, float focusY, float focusZ,
+                         float upX, float upY, float upZ);
 
-				void Update(float dt);
-				void Look();
+                void Update(float dt);
+                void Look();
 
-				//Frustum& GetFrustum();
+                //Frustum& GetFrustum();
 
-				void LockMouse(bool lock);
-				bool IsMouseLocked();
+                void LockMouse(bool lock);
+                bool IsMouseLocked();
 
-				void SetKeyboardInput(Events::KeyboardEvent& event);
-				void SetMouseInfo(const Math::vec2i& mousePosition);
-				void SetMouseInfo(int x = 0, int y = 0);
+                void SetTouchInput(Events::TouchEvent& event);
+                
+                void SetKeyboardInput(Events::KeyboardEvent& event);
+                
+                void SetMouseInfo(const Math::vec2i& mousePosition);
+                void SetMouseInfo(int x = 0, int y = 0);
 
-				const Math::vec3f& GetViewerPosition() const;
-				const Math::vec3f& GetFocusPosition() const;
-				const Math::vec3f& GetUpDirection() const;
+                const Math::vec3f& GetViewerPosition() const;
+                const Math::vec3f& GetFocusPosition() const;
+                const Math::vec3f& GetUpDirection() const;
 
-				void RotateAroundPoint(const Math::vec3f& center, float angle, float x, float y, float z);
-				
-				Math::mat4f getViewMatrix();
+                void RotateAroundPoint(const Math::vec3f& center, float angle, float x, float y, float z);
 
-			  private:
-				void ProcessKeyboardEvents();
+                Math::mat4f getViewMatrix();
 
-				void Move(float speed);
-				void StrafeMove(float speed);
-				void RotateView(float angle, float x, float y, float z);
-				void SetViewByMouse();
+              private:
+                void ProcessKeyboardEvents();
+                void Move(float speed);
+                void StrafeMove(float speed);
+                void RotateView(float angle, float x, float y, float z);
+                void SetViewByMouse();
 
-				//Frustum frustum;
-				Math::vec2i mouseLockedPosition, mousePosition;
-				Math::vec3f position, strafe, focus, up;
-				Math::mat4f viewMatrix;
-				bool activeKeys[6], mouseLocked;
-				float speed, translationSpeed, mouseSensitivity, mouseSpeed, currentXRotation;
-			};
-		}
-	}
+                //Frustum frustum;
+                Math::vec2i mouseLockedPosition, mousePosition;
+                Math::vec3f position, strafe, focus, up;
+                Math::mat4f viewMatrix;
+
+                bool activeKeys[6], mouseLocked;
+                float speed, translationSpeed, mouseSensitivity, mouseSpeed, currentXRotation;
+            };
+        }
+    }
 }
 #endif	/* CAMERA_HPP */
 
