c2d3cd5eb8e86d6dea513855637f8f41f82e7e47
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2015-08-10 03:07:04 +0200

    refs #42 @30min * Fixed problem with inline methods in Process class. * Fixed problem with dependencies for test executables. * New tests for Process class.

--- source/src/NGE/Core/Process.cpp
+++ source/src/NGE/Core/Process.cpp
@@ -4,91 +4,69 @@
 using namespace NGE::Core;
 
 Process::Process() {
-	state = UNINITIALIZED;
+    state = UNINITIALIZED;
 }
 
 Process::~Process() {
-	if (child) {
-		child->OnAbort();
-	}
+    if (child) {
+        child->OnAbort();
+    }
 }
 
-void Process::Success() {
-	if (state == RUNNING || state == PAUSED) {
-		state = SUCCEEDED;
-	} else {
-		log_info("Attempting to success process that isn't running");
-	}
+void Process::OnInit() {
+    state = RUNNING;
 }
 
-void Process::Fail() {
-	if (state == RUNNING || state == PAUSED) {
-		state = FAILED;
-	} else {
-		log_info("Attempting to fail process that isn't running");
-	}
-}
+void Process::OnSuccess() { }
 
-void Process::Pause() {
-	if (state == RUNNING) {
-		state = PAUSED;
-	} else {
-		log_info("Attempting to pause a process that isn't running");
-	}
-}
+void Process::OnFail() { }
 
-void Process::UnPause() {
-	if (state == PAUSED) {
-		state = RUNNING;
-	} else {
-		log_info("Attempting to unpause a process that isn't paused");
-	}
-}
+void Process::OnAbort() { }
 
 Process::State Process::GetState() const {
-	return state;
+    return state;
 }
 
 bool Process::IsAlive() const {
-	return state == RUNNING || state == PAUSED;
+    return state == RUNNING || state == PAUSED;
 }
 
 bool Process::IsDead() const {
-	return state == SUCCEEDED || state == FAILED || state == ABORTED;
+    return state == SUCCEEDED || state == FAILED || state == ABORTED;
 }
 
 bool Process::IsRemoved() const {
-	return state == REMOVED;
+    return state == REMOVED;
 }
 
 bool Process::IsPaused() const {
-	return state == PAUSED;
+    return state == PAUSED;
 }
 
 void Process::AttachChild(StrongProcessPtr child) {
-	if (this->child) {
-		this->child->AttachChild(child);
-	} else {
-		this->child = child;
-	}
+    if (this->child) {
+        this->child->AttachChild(child);
+    } else {
+        this->child = child;
+    }
 }
 
 StrongProcessPtr Process::RemoveChild() {
-	if (child) {
-		// Keep the child from getting destroyed when clear.
-		StrongProcessPtr ptrChild = child;
-		child.reset();
-		return ptrChild;
-	}
+    if (child) {
+        // Keep the child from getting destroyed when clear.
+        StrongProcessPtr ptrChild = child;
+        child.reset();
+        return ptrChild;
+    }
 
-	return StrongProcessPtr();
+    return StrongProcessPtr();
 }
 
 StrongProcessPtr Process::PeekChild() {
-	return child;
+    return child;
 }
 
 void Process::SetState(State state) {
-	this->state = state;
+    this->state = state;
 }
 
--- test/ProcessTest.cpp
+++ test/ProcessTest.cpp
@@ -6,8 +6,51 @@
 #include "NGE/Core/Process.hpp"
 using namespace NGE::Core;
 
-BOOST_AUTO_TEST_CASE(createProcessTest) {
-    Process process;
-	
-	BOOST_CHECK_EQUAL(Process::UNINITIALIZED, process.GetState());
+class TestProcess : public Process {
+  public:
+
+    virtual void OnUpdate(unsigned int deltaTime) { }
+
+};
+
+class TestProcessWrapper : public TestProcess {
+  public:
+    using TestProcess::OnInit;
+    using TestProcess::SetState;
+
+};
+
+BOOST_AUTO_TEST_CASE(CreateProcess) {
+    TestProcess process;
+    BOOST_CHECK_EQUAL(Process::UNINITIALIZED, process.GetState());
+}
+
+BOOST_AUTO_TEST_CASE(OnInit) {
+    TestProcessWrapper process;
+    process.OnInit();
+    BOOST_CHECK_EQUAL(Process::RUNNING, process.GetState());
+}
+
+BOOST_AUTO_TEST_CASE(Success) {
+    TestProcessWrapper process;
+    process.OnInit();
+
+    process.Success();
+    BOOST_CHECK_EQUAL(Process::SUCCEEDED, process.GetState());
+
+    process.SetState(Process::FAILED);
+    process.Success();
+    BOOST_CHECK_EQUAL(Process::FAILED, process.GetState());
+}
+
+BOOST_AUTO_TEST_CASE(Fail) {
+    TestProcessWrapper process;
+    
+    process.SetState(Process::PAUSED);
+    process.Fail();
+    BOOST_CHECK_EQUAL(Process::FAILED, process.GetState());
+    
+    process.SetState(Process::ABORTED);
+    process.Fail();
+    BOOST_CHECK_EQUAL(Process::ABORTED, process.GetState());
 }
