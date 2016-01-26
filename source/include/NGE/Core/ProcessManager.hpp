832813eec615941e5971eaaf1c1473ee966eb22d
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-01-26 18:05:24 +0100

    Refactoring in CMakeList.txt file for tests.

--- source/include/NGE/Core/ProcessManager.hpp
+++ source/include/NGE/Core/ProcessManager.hpp
@@ -25,6 +25,11 @@ namespace NGE {
 		  public:
 			virtual ~ProcessManager();
 
+			unsigned int UpdateProcesses(unsigned int deltaTime) {
+				unsigned int strongProcessResult = UpdateProcesses(deltaTime, processList);
+				unsigned int rawProcessResult = UpdateProcesses(deltaTime, rawProcessList);
+			}
+			
 			/**
 			 * The process update tick. Called every logic tick. This function returns
 			 * the number of process chains that succeeded in the upper 32 bits and
@@ -33,15 +38,81 @@ namespace NGE {
 			 * @param deltaTime 
 			 * @return 
 			 */
-			unsigned int UpdateProcesses(unsigned int deltaTime);
+			template <typename T> unsigned int UpdateProcesses(unsigned int deltaTime, std::list<T>& list) {
+				unsigned short int successCount = 0;
+				unsigned short int failCount = 0;
+
+				auto it = list.begin();
+				while (it != list.end()) {
+					// Grab the next process.
+					T currentProcess = (*it);
+
+					// Save the iterator and increment the old one in case we need to
+					// remove this process from list.
+					auto thisIt = it;
+					++it;
+
+					// Process is uninitialised, so initialise it.
+					if (currentProcess->GetState() == Process::UNINITIALISED) {
+						currentProcess->OnInit();
+					}
+
+					// Give the process an update tick if it's running.
+					if (currentProcess->GetState() == Process::RUNNING) {
+						currentProcess->OnUpdate(deltaTime);
+					}
+
+					// Check to see if the process is dead.
+					if (currentProcess->IsDead()) {
+						// Run the appropriate exit function.
+						switch (currentProcess->GetState()) {
+							case Process::SUCCEEDED:
+							{
+								currentProcess->OnSuccess();
+								StrongProcessPtr child = currentProcess->RemoveChild();
+								if (child) {
+									AttachProcess(child);
+								} else {
+									// Only counts if the whole chain is completed.
+									++successCount;
+								}
+								break;
+							}
+
+							case Process::FAILED:
+							{
+								currentProcess->OnFail();
+								++failCount;
+								break;
+							}
+
+							case Process::ABORTED:
+							{
+								currentProcess->OnAbort();
+								++failCount;
+								break;
+							}
+
+							default:
+								break;
+						}
+
+						// Remove process and destroy it.
+						list.erase(thisIt);
+					}
+				}
+
+				return ((successCount << 16) | failCount);
+			}
+
 			WeakProcessPtr AttachProcess(NGE::Core::StrongProcessPtr process);
-			
+
 			template <typename T> void AbortAllProcesses(bool immediate, std::list<T>& list) {
 				auto it = list.begin();
 				while (it != processList.end()) {
 					auto tempIt = it;
 					++it;
-					
+
 					T process = *tempIt;
 					if (process->IsAlive()) {
 						process->SetState(Process::ABORTED);
---
+++ test/Core/ProcessManagerTest.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <memory>
+#include <boost/test/unit_test.hpp>
+#include "NGE/Core/ProcessManager.hpp"
+using namespace NGE::Core;
+
+BOOST_AUTO_TEST_CASE(testProcessManager) {
+	
+}
