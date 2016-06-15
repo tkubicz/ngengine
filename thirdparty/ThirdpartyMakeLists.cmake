75413c8b77587e5ec8f03164fa9b5dcd7697c0c6
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-06-15 18:42:43 +0200

    Small modifications in build system.

--- thirdparty/ThirdpartyMakeLists.cmake
+++ thirdparty/ThirdpartyMakeLists.cmake
@@ -4,6 +4,6 @@ cmake_minimum_required (VERSION 3.0)
 include_directories("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/kaguya/include/")
 include_directories("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/pugixml/src/")
 include_directories("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/Catch/include/")
-
 include_directories("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cppformat/")
-add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cppformat" thirdparty/cppformat)
+
+add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cppformat")
